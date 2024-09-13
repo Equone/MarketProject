74d3f3fc8c9f47b8f8d277adb005893b082e2a91
Nathan Boudol <149159830+Equone@users.noreply.github.com>
2024-09-13 19:34:00 +0200

    Update alphavantage_api.py

--- src/alphavantage_api.py
+++ src/alphavantage_api.py
@@ -7,17 +7,17 @@ My API Key : F1CSEY3K0H1XDJKO
 
 def get_days_elapsed_since_dividends(symbol, api_key):
     """
-    Récupère les jours écoulés depuis les dates des dividendes non nuls pour une entreprise donnée.
+    Retrieves the number of days that have elapsed since the dates of non-zero dividends for a given company.
 
-    :param symbol: Le symbole boursier de l'entreprise (par exemple, 'MSFT' pour Microsoft).
-    :param api_key: La clé API pour accéder à Alpha Vantage.
-    :return: Une liste des jours écoulés depuis chaque date de dividende non nulle.
+    :param symbol: The stock symbol of the company (e.g., 'MSFT' for Microsoft).
+    :param api_key: The API key to access Alpha Vantage.
+    :return: A list of the number of days elapsed since each dividend date with non-zero amounts.
     """
     url = f'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol={symbol}&apikey={api_key}'
     r = requests.get(url)
     data = r.json()
 
-    # Parcourir les données pour trouver les dates avec des dividendes non nuls
+    # Loop through the data to find dates with non-zero dividends
     dates_with_dividends = []
 
     for date, metrics in data["Weekly Adjusted Time Series"].items():
@@ -25,14 +25,14 @@ def get_days_elapsed_since_dividends(symbol, api_key):
         if dividend_amount > 0:
             dates_with_dividends.append((date, dividend_amount))
 
-    # Liste des dates de dividendes
+    # List of dividend dates
     dividend_dates = [dates_with_dividends[i][0] for i in range(len(dates_with_dividends))]
     time_elapsed_since_div = []
 
-    # Obtenir la date d'aujourd'hui
+    # Get today's date
     today = datetime.today()
 
-    # Parcourir chaque date et calculer les jours écoulés
+    # Loop through each date and calculate the days elapsed
     for date_str in dividend_dates:
         dividend_date = datetime.strptime(date_str, "%Y-%m-%d")
         days_elapsed = (today - dividend_date).days
